e0d937dbf9119e85cd10381bfe1e6a3d5a83bda8
Matthew Wilkerson <mwilkers@redhat.com>
2021-01-07 14:01:48 -0700

    Update to use URL picker and configure via ENV Vars

--- src/extension.ts
+++ src/extension.ts
@@ -7,29 +7,76 @@ export const greetingViewType = 'greeting';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// vscode.window.registerWebviewPanelSerializer(greetingViewType, new GreetingPageSerializer());
-
-	let disposable = vscode.commands.registerCommand('greeting-extension.open-webview', () => {
-		openWebView();
+	let disposable = vscode.commands.registerCommand('openurl-extension.open-webview', () => {
+		openUrlList();
 	});
 
 	context.subscriptions.push(disposable);
 
-	openWebView();
+	openUrlList();
 }
 
-function openWebView() {
-    // Open Greeting tab
-	const panel = vscode.window.createWebviewPanel(greetingViewType, "Greeting",
-		{
-			viewColumn: vscode.ViewColumn.One,
-			preserveFocus: false
-		},
-		{
+function openUrlList() {
+
+	let items: vscode.QuickPickItem[] = [];
+
+	var commands: any[] = [];
+
+	// Get variables set
+	var child_process = require("child_process");
+	var env_vars = child_process.execSync("env | grep CUSTOM_OPEN_URL_");
+	var env_vars_arr: any[];
+	var conf_arr = [];
+	var  key = ""
+	var value = ""
+
+	env_vars_arr = env_vars.toString().split(/\r?\n/);
+	env_vars_arr.pop();
+	console.log(env_vars_arr.length)
+	console.log(env_vars_arr)
+
+	for (let index = 0; index < env_vars_arr.length; index++) {
+
+		value = env_vars_arr[index].split("=")[1]
+		console.log(value)
+		conf_arr = value.split("|")
+		key = conf_arr[0].toString()
+
+		items.push({
+			label: conf_arr[0],
+			description: conf_arr[1],
+		})
+
+		// @ts-ignore
+		commands[key] = {
+			description: conf_arr[1],
+			url: conf_arr[2]
+		};
+	}
+	// @ts-ignore
+	console.log(commands)
+
+	vscode.window.showQuickPick(items).then(selection => {
+		// the user canceled the selection
+		if (!selection) {
+			return;
+		}
+		else {
+			// @ts-ignore
+			const url = commands[selection.label.toString()].url;
+			// @ts-ignore
+			const title = commands[selection.label.toString()].description;
+			const style = "position: absolute; width: calc(100% - 30px); height: calc(100% - 10px);";
+			const panel = vscode.window.createWebviewPanel(greetingViewType, title,
+				{
+					viewColumn: vscode.ViewColumn.One,
+					preserveFocus: false
+				},
+				{
+				});
+			panel.webview.html = `<iframe src='${url}' style='${style}'></iframe>`;
+		}
 	});
-	
-	panel.webview.html = "Hello";
 }
 
 // this method is called when your extension is deactivated
